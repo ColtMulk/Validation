d557fd288c89d327d725461f66594bd7c67973fb
coltmulk <coltmulk@tamu.edu>
2021-11-09 14:19:01 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import argparse
+import cv2
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+path = r'C:\Users\ColtonMulkey\Desktop\CodingProjects\Capstone\Validation\Shelf.jpg'
+image = cv2.imread(path)
+image = cv2.resize(image, (1008,756))
+output = image.copy()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#cv2.imshow("output", gray)
+#cv2.waitKey(0)
+
+
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=40, minRadius=150, maxRadius=170)
+
+print("hello")
+if circles is not None:
+	# convert the (x, y) coordinates and radius of the circles to integers
+	circles = np.round(circles[0, :]).astype("int")
+	# loop over the (x, y) coordinates and radius of the circles
+	for (x, y, r) in circles:
+		# draw the circle in the output image, then draw a rectangle
+		# corresponding to the center of the circle
+		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+	# show the output
+
+	cv2.imshow("output", np.hstack([image, output]))
+	cv2.waitKey(0)
+else:
+	print("No circles found")
